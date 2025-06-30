69ab82571627c5c1934f3c239e5477b9759e81d3
simonnemcic <simonnemci35@gmail.com>
2025-06-30 19:42:49 +0200

    update: added router to index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const app = express();
 let port = 3000;
 
+const characterRoutes = require('./routes/character.routes');
+app.use('/api', characterRoutes);
+
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
